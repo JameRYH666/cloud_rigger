16118132da621e84053b9f1946a051284b5041b2
huayang.bai <1053091115@qq.com>
2020-09-12 14:15:01 +0800

    修改了项目名称

--- cloud-admin/cloud-admin-model/src/main/java/com/jeeadmin/entity/CloudMeetingEnclosure.java
+++ cloud-admin/cloud-admin-model/src/main/java/com/jeeadmin/entity/CloudMeetingEnclosure.java
@@ -6,14 +6,11 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
-import java.util.Date;
-
 /**
  * @author Seven Lee
- * @description
- *      会议附件关系表
+ * @description 会议附件关系表
  * @date 2020/9/8
-**/
+ **/
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
@@ -28,25 +25,4 @@ public class CloudMeetingEnclosure extends BaseModel<CloudMeetingEnclosure> {
      * 附件id
      */
     private Long enclosureId;
-
-    /**
-     * 创建用户
-     */
-    private String createUser;
-
-    /**
-     * 创建时间
-     */
-    private Date createDate;
-
-    /**
-     * 更新用户
-     */
-    private String updateUser;
-
-    /**
-     * 更新时间
-     */
-    private Date updateDate;
-
 }
