91c9a3f94575002b95c9c103ae46f2db8f6dc964
huayang.bai <1053091115@qq.com>
2020-10-09 22:23:48 +0800

    修改了分页问题

--- cloud-common/cloud-rigger-frame/src/main/java/com/jeerigger/frame/page/PageHelper.java
+++ cloud-common/cloud-rigger-frame/src/main/java/com/jeerigger/frame/page/PageHelper.java
@@ -1,8 +1,10 @@
 package com.jeerigger.frame.page;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class PageHelper<T> {
     /**
      * 当前页数
@@ -16,9 +18,6 @@ public class PageHelper<T> {
      * 查询条件
      */
     private T data;
-
-    public PageHelper(T current, T size) {
-    }
 
     /**
      * 设置每页记录数默认值
